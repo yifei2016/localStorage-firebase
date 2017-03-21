dae38190d9b63a2d57ec5d5758468221f14b530b
Yifei Wang <hello.yifei@icloud.com>
2017-03-21 22:03:09 +0100

    promise added

--- localStorage.js
+++ localStorage.js
@@ -1,43 +1,89 @@
-// let submit = document.getElementById('submit');
-
 let forget = document.getElementById('forget');
-
 function submitMessage(){
   let firstN = document.getElementById('firstN');
   let lastN = document.getElementById('lastN');
-  let fullName =  firstN.value + lastN.value;
+  let fullName =  firstN.value + " "+ lastN.value;
   localStorage.setItem('whatever', fullName);
   if (localStorage.getItem('whatever') !== null) {
     let paragraf = document.getElementById("paragraf");
-    paragraf.innerHTML = 'welcome ' + localStorage.getItem('whatever'); // a.whatever
+    paragraf.innerHTML = 'welcome: ' + localStorage.getItem('whatever'); // a.whatever
   } else {
     let paragraf = document.getElementById("paragraf");
-    paragraf.innerHTML = 'please enter your name';
-}
-  let exampleTextarea = document.getElementById("exampleTextarea");
-  let message = {
-    name: fullName,
-    text: exampleTextarea.value,
-    time: moment().format('MM-DD-YYYY, h:mm:ss')
+    paragraf.innerHTML = 'please enter your name:';
   }
-  //update an existing record
-  // let messageId = "-Kfc5UVtW1hFT1Y36JTC"
-  // firebase.database().ref('message/' + messageId ).set(message);
-  //creat a new record
-  // firebase.database().ref('message/').push().set(message);
-  let messageId = "-Kfc5dxeIK0hwBauZ5-Z";
-   firebase.database().ref.child(messageId).remove();
-}
-//read a record
+  //curl 'https://docs-examples.firebaseio.com/rest/saving-data/auth-example.json?auth=CREDENTIAL'
+  let exampleTextarea = document.getElementById("exampleTextarea");
+  let fb = firebase.database();
+  // fb.ref().child('message/')
+  //   .once('value')
+  //   .then(function() {
+  //
+  //   })
+  //   .then(function(){
+  //
+  //   })
+  //   .then(function(){
+  //
+  //   })
+  //   .catch(function(){
+  //
+  //   })
+  //   .finally(function(){
+  //
+  //   })
+  fb.ref().child('message/').once('value')
+    .then(function(snapshot){
+      let allMessages = snapshot.val();
+      if(allMessages === null) {
+        allMessages={}
+      }
+      let count = Object.keys(allMessages).length;
+      let message = {
+        name: fullName,
+        text: exampleTextarea.value,
+        time: moment().format('MM-DD-YYYY, h:mm:ss'),
+        id: count+1,
+        like: 0
+      }
+      return fb.ref('message/'+ message.id).set(message);
+    })
+    .then(function(){
+      let table = document.getElementById("table");
+      table.innerHTML = '';
+      return listMessages();
+    })
+    .catch(function(err){
 
+    })
+  // fb.ref().child('message/').once('value', function(snapshot) {
+  //  firebase.database().ref('message/').on('value', function(snapshot) { }// });
+}
+
+function updateLike(messageId, likePoints){
+  firebase.database().ref('message/' + messageId + 'like/').set(likePoints)
+}
+
+//delete a record
+//fb.ref().child("message").remove()
+
+// fb.ref('message/').on('value', function(snapshot) {
+//   let allMessages = snapshot.val();}
+
+//update an existing record
+// let messageId = "-Kfc5UVtW1hFT1Y36JTC"
+// firebase.database().ref('message/' + messageId ).set(message);
+
+// let messageId = "-Kfc5dxeIK0hwBauZ5-Z";
+//  firebase.database().ref.child(messageId).remove();
+//read a record
 function animateInterval(percent){
   var myVar = setInterval(frame, 10);
   let progress = document.getElementsByClassName("progress-bar")[0];
   progress.style.width = `0%`;
   progress.style.display = "block";
   function frame(){
-    if (percent == 100) {
-      progress.style.width = `100%`;
+    if(percent == 100){
+      progress.style.width = '100%';
       progress.style.display = 'none';
       clearInterval(myVar);
     }else{
@@ -51,52 +97,72 @@ function listMessages(){
   let table = document.getElementById("table");
   table.innerHTML = '';
   let progress = document.getElementsByClassName("progress-bar")[0];
-  animateInterval(0)
-
-  firebase.database().ref('message/').on('value', function(snapshot) {
-    let allMessages = snapshot.val();
-    Object.keys(allMessages).reverse().forEach(function(key){
-      let message = allMessages[key];
-      let tr = document.createElement('tr');
-      tr.innerHTML = `<td>${message.name}</td>
-      <td>${message.text}
-      <button style="border:none; background-color:Transparent;"><i class="fa fa-thumbs-up fa-2x" aria-hidden="true" style="color:blue;"></i></button>
-      <button style="border:none; background-color:Transparent;"><i class="fa fa-thumbs-down fa-2x" aria-hidden="true" style="color:red;"></i></button></td>
-      <td>${message.time}</td>`;
-      table.appendChild(tr);
-      setTimeout(function(){
-        animateInterval(100)
-      }, 2000);
-    });
-  });
+  animateInterval(0);
+  firebase.database().ref('message/').once('value')
+  .then(function(snapshot){
+      let allMessages = snapshot.val();
+      if(allMessages === null){
+        allMessages={}
+      }
+      Object.keys(allMessages).reverse().forEach(function(key){
+        let mes = allMessages[key];
+        debugger
+        let tr = document.createElement('tr');
+        tr.innerHTML = `<td>${mes.name}</td>
+        <td>${mes.text}
+        <button style="border:none; background-color:Transparent;">
+        <i class="fa fa-thumbs-up fa-2x" aria-hidden="true" style="color:pink;" id="upVote"></i>
+        <p id="point"></p></button>
+        <button  style="border:none; background-color:Transparent;">
+        <i class="fa fa-thumbs-down fa-2x" aria-hidden="true" style="color:black;" id="downVote"></i>
+        </button>
+        </td>
+        <td>${mes.time}</td>
+        <td>${mes.id}</td>`;
+        table.appendChild(tr);
+        let upVote = document.getElementById("upVote");
+        let downVote = document.getElementById("downVote");
+        let point = document.getElementById("point");
+        upVote.addEventListener("click",function(event){
+          mes.like += 1;
+          point.innerHTML = mes.like;
+          updateLike(mes.id,mes.like);
+        })
+        let fa = function(event){
+          mes.like -= 1;
+          point.innerHTML = mes.like;
+          updateLike(mes.id,mes.like);
+        }
+        downVote.addEventListener("click",fa)
+        setTimeout(function(){
+          animateInterval(100)
+        }, 2000);
+      });
+  })
+  .catch(function(err){
+    debugger
+  })
+   // firebase.database().ref('message/').on('value', function(snapshot) {});
 }
-//delete a record
 
 function forgetButton(){
-    localStorage.removeItem('whatever'); // delete a["whatever"]
-    let paragraf = document.getElementById("paragraf");
-    paragraf.innerHTML="please enter your name";
+  localStorage.removeItem('whatever'); // delete a["whatever"]
+  let paragraf = document.getElementById("paragraf");
+  paragraf.innerHTML="please enter your name";
 }
-
-
 
 window.onload = function(){
 
 
 }
-//
-//   // submit.addEventListener('click', function() {
-//   //   // a = {}          // localStorage = {}
-//   //   // a.whatever = 2; // localStorage.setItem("whatever",2);
-//   //   // a.wha = 3;      // localStorage.setItem("wha",3);
-//   //   localStorage.setItem('whatever', firstN.value +lastN.value);
-//   //   localStorage.setItem('wha', firstN.value +lastN.value);
-//   //   localStorage.setItem('wh', firstN.value +lastN.value);
-//   //    //diff key for diff value to save
-//   // })
-//
-//   //
-//   // }
-//   // // console.log(localStorage.getItem('whatever'))
-//
-// }
+
+// submit.addEventListener('click', function() {
+//   // a = {}          // localStorage = {}
+//   // a.whatever = 2; // localStorage.setItem("whatever",2);
+//   // a.wha = 3;      // localStorage.setItem("wha",3);
+//   localStorage.setItem('whatever', firstN.value +lastN.value);
+//   localStorage.setItem('wha', firstN.value +lastN.value);
+//   localStorage.setItem('wh', firstN.value +lastN.value);
+//    //diff key for diff value to save
+// })
+// console.log(localStorage.getItem('whatever'))
